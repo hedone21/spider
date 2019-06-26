7c76f1dc76d41565b550f18f81116f8ccaae3ba9
minyoung.go <hedone21@gmail.com>
2019-06-26 22:24:34 +0900

    common/log.h: Add log funcions

---
+++ common/log.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2019 Minyoung.Go <hedone21@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __LOG_H__
+#define __LOG_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+
+#define spider_log(fmt, ...) 						\
+	do {								\
+		fprintf(stderr, "[%s:%d] " fmt, __func__, __LINE__, 	\
+			## __VA_ARGS__);				\
+	}while(0)
+
+#define spider_dbg(fmt, ...) 						\
+	do {								\
+		fprintf(stderr, "[%s:%d] " fmt, __func__, __LINE__, 	\
+			## __VA_ARGS__);				\
+	}while(0)
+
+#define spider_err(fmt, ...) 						\
+	do {								\
+		fprintf(stderr, "[%s:%d] " fmt, __func__, __LINE__, 	\
+			## __VA_ARGS__);				\
+	}while(0)
+
+#endif /* __LOG_H__ */
+
