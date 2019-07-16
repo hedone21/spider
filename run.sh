0c36b9751fff0eebbdcbabca674a239a9c95a1e2
minyoung.go <hedone21@gmail.com>
2019-07-16 11:06:21 +0900

    Change compositor dependency from weston to wlroots
    Take tinywl as a skeleton compositor
    (tinywl is a sample compositor of wlroots)

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-WESTON_CONFIG=$PWD"/config.txt"
-
-echo "[shell]" > $WESTON_CONFIG
-#echo "client="$PWD"/build/client-webkit/spider" >> $WESTON_CONFIG
-
-weston -c $WESTON_CONFIG --shell=$PWD"/build/server/spider-server.so"
+#$PWD/build/server/spider-server -s [client]
+$PWD/build/server/spider-server
--- server/server.h
+++ server/server.h
@@ -1,4 +1,6 @@
 /*
+ * Copyright (c) 2014 Jari Vetoniemi
+ * Copyright (c) 2017, 2018 Drew DeVault
  * Copyright (c) 2019 Minyoung.Go <hedone21@gmail.com>
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -26,16 +28,72 @@
 #include <wayland-server.h>
 #include <libweston-6/compositor.h>
 
-struct spider_server {
-	struct wl_display *display;
-	struct wl_event_loop *event_loop;
-
-	struct weston_compositor *compositor;	
-	struct weston_layer background_layer;
-
-	char *client;
+/* For brevity's sake, struct members are annotated where they are used. */
+enum spider_cursor_mode {
+	spider_CURSOR_PASSTHROUGH,
+	spider_CURSOR_MOVE,
+	spider_CURSOR_RESIZE,
 };
 
-int server_initialize(struct spider_server *server);
+struct spider_server {
+	struct wl_display *wl_display;
+	struct wlr_backend *backend;
+	struct wlr_renderer *renderer;
+
+	struct wlr_xdg_shell *xdg_shell;
+	struct wl_listener new_xdg_surface;
+	struct wl_list views;
+
+	struct wlr_cursor *cursor;
+	struct wlr_xcursor_manager *cursor_mgr;
+	struct wl_listener cursor_motion;
+	struct wl_listener cursor_motion_absolute;
+	struct wl_listener cursor_button;
+	struct wl_listener cursor_axis;
+	struct wl_listener cursor_frame;
+
+	struct wlr_seat *seat;
+	struct wl_listener new_input;
+	struct wl_listener request_cursor;
+	struct wl_list keyboards;
+	enum spider_cursor_mode cursor_mode;
+	struct spider_view *grabbed_view;
+	double grab_x, grab_y;
+	int grab_width, grab_height;
+	uint32_t resize_edges;
+
+	struct wlr_output_layout *output_layout;
+	struct wl_list outputs;
+	struct wl_listener new_output;
+};
+
+struct spider_output {
+	struct wl_list link;
+	struct spider_server *server;
+	struct wlr_output *wlr_output;
+	struct wl_listener frame;
+};
+
+struct spider_view {
+	struct wl_list link;
+	struct spider_server *server;
+	struct wlr_xdg_surface *xdg_surface;
+	struct wl_listener map;
+	struct wl_listener unmap;
+	struct wl_listener destroy;
+	struct wl_listener request_move;
+	struct wl_listener request_resize;
+	bool mapped;
+	int x, y;
+};
+
+struct spider_keyboard {
+	struct wl_list link;
+	struct spider_server *server;
+	struct wlr_input_device *device;
+
+	struct wl_listener modifiers;
+	struct wl_listener key;
+};
 
 #endif /* __SERVER_H__ */
