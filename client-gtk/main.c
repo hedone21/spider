bdf95d4e13e56abb2df8c001459bbbb3dde88e8d
minyoung.go <hedone21@gmail.com>
2019-07-28 20:46:29 +0900

    client-gtk/main.c: Add licence and match indents

--- client-gtk/main.c
+++ client-gtk/main.c
@@ -1,46 +1,72 @@
+/*
+ * Copyright (c) 2019 Minyoung.Go <hedone21@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/* 
+ * This code is based on https://github.com/aragua/gtkbrowser.
+ */
+
 #include <gtk/gtk.h>
 #include <webkit2/webkit2.h>
 
-static void destroyWinCb(GtkWidget* widget, GtkWidget* window)
+static void destroy_win_cb(GtkWidget* widget, GtkWidget* window)
 {
-    gtk_main_quit();
+	gtk_main_quit();
 }
 
-static gboolean closeWebCb(WebKitWebView* webView, GtkWidget* window)
+static gboolean close_web_Cb(WebKitWebView* webView, GtkWidget* window)
 {
-    gtk_widget_destroy(window);
-    return TRUE;
+	gtk_widget_destroy(window);
+	return TRUE;
 }
 
 int main(int argc, char* argv[])
 {
-    GtkWidget *win;
-    WebKitWebView *web;
-    char *url = "http://www.google.com/";
- 
-    gtk_init(&argc, &argv);
+	GtkWidget *win;
+	WebKitWebView *web;
+	char *url = "http://www.google.com/";
 
-    if (argc == 2)
-        url = argv[1];
+	gtk_init(&argc, &argv);
 
-    win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_default_size(GTK_WINDOW(win), 1280, 720);
-    gtk_window_fullscreen(GTK_WINDOW(win));
+	if (argc == 2)
+		url = argv[1];
 
-    web = WEBKIT_WEB_VIEW(webkit_web_view_new());
+	win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_default_size(GTK_WINDOW(win), 1280, 720);
+	gtk_window_fullscreen(GTK_WINDOW(win));
 
-    gtk_container_add(GTK_CONTAINER(win), GTK_WIDGET(web));
+	web = WEBKIT_WEB_VIEW(webkit_web_view_new());
 
-    g_signal_connect(win, "destroy", G_CALLBACK(destroyWinCb), NULL);
-    g_signal_connect(web, "close", G_CALLBACK(closeWebCb), win);
+	gtk_container_add(GTK_CONTAINER(win), GTK_WIDGET(web));
 
-    webkit_web_view_load_uri(web, url);
+	g_signal_connect(win, "destroy", G_CALLBACK(destroy_win_cb), NULL);
+	g_signal_connect(web, "close", G_CALLBACK(close_web_Cb), win);
 
-    gtk_widget_grab_focus(GTK_WIDGET(web));
+	webkit_web_view_load_uri(web, url);
 
-    gtk_widget_show_all(win);
+	gtk_widget_grab_focus(GTK_WIDGET(web));
 
-    gtk_main();
+	gtk_widget_show_all(win);
 
-    return 0;
+	gtk_main();
+
+	return 0;
 }
