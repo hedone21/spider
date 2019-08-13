1b5663f4df8fc75fbed7315e25a036522efe28ae
minyoung.go <minyoung.go@lge.com>
2019-08-13 15:53:20 +0900

    run.sh: change running file

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ export SPIDER_WEB_URL=http://localhost:2580/
 export SPIDER_CLIENT_SERVER_PATH=web
 
 #$PWD/build/server/spider-server -s $PWD/build/client-shell/client -V
-$PWD/build/spider/spider-compositor \
+$PWD/build/spider/spider \
 	-s $PWD/build/shell/client \
 	-r $PWD/build/server/server
