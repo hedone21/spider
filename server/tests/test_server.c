1d84109ccfb6def3b8787b52caa4bf5357181f62
minyoung.go <hedone21@gmail.com>
2020-11-23 21:29:42 +0900

    Temp commit

--- server/tests/test_server.c
+++ server/tests/test_server.c
@@ -16,9 +16,6 @@ static void test_server_operation() {
     spider_server_free(&server);
 }
 
-static void test_server_callback(struct spider_server *server, void *data) {
-}
-
 static void test_server_operation_with_backend() {
     struct spider_server *server = NULL;
     struct spider_backend *backend = NULL;
@@ -37,7 +34,42 @@ static void test_server_operation_with_backend() {
     spider_server_free(&server);
 }
 
+static void test_server_callback(struct spider_server *server, void *data) {
+}
+
+static void test_server_operation_with_callbacks() {
+    struct spider_server *server = NULL;
+    struct spider_backend *backend = NULL;
+    struct spider_backend_server *backend_server = NULL;
+
+    backend = spider_backend_create_with_sopath("server/spider_backend_mock.so");
+    assert(backend != NULL);
+
+    /* Types of callbacks
+     * [Server]
+     * - START_SERVER (struct spider_server *server)
+     * - STOP_SERVER (struct spider_server *server)
+     * 
+     * [Client]
+     * - NEW_CLIENT (struct spider_server *server, struct spider_client *client)
+     * - DEL_CLIENT (struct spider_server *server, struct spider_client *client))
+     *
+     * [Window]
+     * - NEW_WINDOW (struct spider_server *server, unsigned int x, unsigned int y, unsigned int w, unsigned int h)
+     */
+
+    server = spider_server_create();
+    assert(server != NULL);
+
+    spider_server_add_backend(server, backend);
+
+    spider_server_run(server);
+
+    spider_server_free(&server);
+}
+
 int main() {
     test_server_operation();
     test_server_operation_with_backend();
+    test_server_operation_with_callbacks();
 }
